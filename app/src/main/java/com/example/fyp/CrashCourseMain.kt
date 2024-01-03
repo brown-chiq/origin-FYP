64a755d147b71995d84ee705c017307c49a91310
brownn_chiq <samy0005@e.ntu.edu.sg>
2024-01-04 01:31:43 +0800

    backbutton added

--- app/src/main/java/com/example/fyp/CrashCourseMain.kt
+++ app/src/main/java/com/example/fyp/CrashCourseMain.kt
@@ -1,13 +1,31 @@
 package com.example.fyp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
+import com.example.fyp.R.id.back_button
+import com.example.fyp.R.id.intro_button
 import com.example.fyp.R.layout.activity_crash_course_main
 
 class CrashCourseMain : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_crash_course_main)
+
+        var introButton = findViewById<Button>(intro_button)
+        introButton.setOnClickListener {
+            val intent = Intent(this, CrashCourseIntroTopic::class.java)
+            startActivity(intent)
+        }
+
+        var backButton = findViewById<ImageView>(back_button)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            @Suppress("DEPRECATION")
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        }
     }
 }
